00b268bf50b178d9153b7d731bdd64b0d06e040f
Reto Schläpfer <reto.schlaepfer.rs@gmail.com>
2014-10-28 14:46:09 +0100

    finished presentation

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ reveal-boilerplate
 ==================
 
 Boilerplate for slides with reveal.js 
+
+npm install
+
+    node server.js
+
+or
+
+    wepack
+
+to build a presentation
+
+alter the PRESENTATION variable in webpack.config to switch between presentations
--- slides-loader.js
+++ slides-loader.js
@@ -8,3 +8,17 @@ require('script!./lib/js/reveal.min.js');
 
 require('./presentations/' + PRESENTATION + '/entry.js');
 
+window.Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: false,
+    theme: window.Reveal.getQueryHash().theme, // available themes are in /css/theme
+    transition: 'linear' // default/cube/page/concave/zoom/linear/fade/none
+});
+
+require.ensure([], function() {
+    require('script!./plugin/highlight/highlight.js');
+    window.hljs.initHighlightingOnLoad();
+    require('script!./plugin/zoom-js/zoom.js');
+});
